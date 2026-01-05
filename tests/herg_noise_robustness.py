bdeabfaeb3b79b4928db7f0f0212e470d1fafcc8
adpunt <apunt@hmc.edu>
2026-01-05 00:13:19 +0000

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! Basic framework with ESOL, QM9, TCGA, and sabdab

--- tests/herg_noise_robustness.py
+++ tests/herg_noise_robustness.py
@@ -261,7 +261,7 @@ def main():
     # Load hERG
     print("\nLoading hERG dataset...")
     data = load_herg()
-    print(data[:10])
+    print(data)
     
     train_smiles = data['train']['smiles']
     train_labels = np.array(data['train']['labels'])
